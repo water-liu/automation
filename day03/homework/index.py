c0921acb6f689d06cc47df641aa574dd755f9893
water-liu <liuyang1990vip@163.com>
2018-08-15 18:35:00 +0800

    day3 作业构想

--- day03/homework/index.py
+++ day03/homework/index.py
@@ -1,3 +1,29 @@
 #!/usr/bin/env python
 # _*_coding:utf-8_*_
 
+
+# 登录
+def login(username, passwd):
+    pass
+
+
+# 还款
+def repayment():
+    pass
+
+
+# 取现
+def withdrawal():
+    pass
+
+
+# 查询
+def query():
+    pass
+
+
+# 转账
+def transfer():
+    pass
+
+
