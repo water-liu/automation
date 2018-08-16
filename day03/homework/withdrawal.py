5d27656affe7d72b9019d8441a48d73a888b4a14
water-liu <liuyang1990vip@163.com>
2018-08-17 00:49:17 +0800

    day3--作业思路

--- day03/homework/withdrawal.py
+++ day03/homework/withdrawal.py
@@ -14,9 +14,10 @@ def withdrawal(username, consumetyep, money, services):
     updatetime = time.strftime('%Y-%m-%d %H:%M:%S')
     if services < 5:
         services = 5
-
+# 更改列表以及文件内容
     pass
 
 
 def maincrash():
+    ## 判断金额是否正确
     pass
