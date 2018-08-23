c12a09210d57357ddb21886d29befa3fb7259159
water-liu <liuyang1990vip@163.com>
2018-08-23 18:40:19 +0800

    day4开始

---
+++ day4/index.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+request = 'gt' if 1 < 3 else 'lt'
+print(request)
---
+++ day4/main/index.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+str1 = 'demo'
+str2 = 'foo'
+
+module = __import__()
